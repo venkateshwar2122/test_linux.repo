51b94aa6b5ae862cbad6b359a4090cbaf7640718
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2026-04-23 23:22:17 +0530

    Create Column.java

---
+++ dto/Column.java
@@ -0,0 +1,14 @@
+package com.example.nifi.dto;
+
+public class Column {
+
+    private String columnName;
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public void setColumnName(String columnName) {
+        this.columnName = columnName;
+    }
+}
