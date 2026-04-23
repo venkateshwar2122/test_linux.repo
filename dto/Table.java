9ae2bc4030602f964cfdc07ed937b856953867d9
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2026-04-23 23:21:55 +0530

    Create  Table.java

---
+++ dto/Table.java
@@ -0,0 +1,25 @@
+package com.example.nifi.dto;
+
+import java.util.List;
+
+public class Table {
+
+    private String tableName;
+    private List<Column> columns;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public List<Column> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Column> columns) {
+        this.columns = columns;
+    }
+}
