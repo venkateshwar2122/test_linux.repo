11730580e671e57347543b38d1bde7a37113d332
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2026-04-23 23:20:42 +0530

    Create  Connection.java

---
+++ dto/Connection.java
@@ -0,0 +1,14 @@
+package com.example.nifi.dto;
+
+public class Connection {
+
+    private Config config;
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+}
